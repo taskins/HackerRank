7a4e521bb4f4432e08e5303619f6af10144122dc
taskins <selengunaytaskin@gmail.com>
2016-07-05 17:57:46 -0700

    initial

---
+++ PrintInReverse/Solution.cs
@@ -0,0 +1,42 @@
+/*
+  Print elements of a linked list in reverse order as standard output
+  head pointer could be NULL as well for empty list
+  Node is defined as
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+#include <stack>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void ReversePrint(Node *head)
+{
+  // This is a "method-only" submission.
+  // You only need to complete this method.
+
+    Node *current = head;
+    stack <int> printStack; // initialize a stack to hold
+    if(head != NULL)
+       {
+    while (current->next != NULL) // put items in a stack
+        {
+        printStack.push(current->data);
+        current = current->next;
+    }
+
+    printStack.push(current->data);// push the last node
+
+    while(!printStack.empty())// pop and display
+        {
+          cout << printStack.top() << endl;
+          printStack.pop();
+        }
+
+   }
+
+}
